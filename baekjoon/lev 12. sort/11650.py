a33338bc7fbd4299ffac585e26f1a341eb527a3a
dlrtn <87226112+dlrtn@users.noreply.github.com>
2021-11-02 17:41:48 +0900

    2021.11. 2.

---
+++ baekjoon/lev 12. sort/11650.py
@@ -0,0 +1,8 @@
+N = int(input())
+nums = []
+for i in range(N):
+    [a, b] = map(int, input().split())
+    nums.append([a, b])
+nums = sorted(nums)
+for i in range(N):
+    print(nums[i][0], nums[i][1])
