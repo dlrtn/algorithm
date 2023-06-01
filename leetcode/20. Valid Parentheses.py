a95d609757bc16770a5a987aa6bda7e5c5437174
dlrtn <dlrtn751@gmail.com>
2023-06-01 19:00:14 +0900

    2023. 06. 01.

---
+++ leetcode/20. Valid Parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        opening_brackets = {'(', '[', '{'}
+        closing_brackets = {')': '(', ']': '[', '}': '{'}
+
+        for i in s:
+            if i in opening_brackets:
+                stack.append(i)
+            elif i in closing_brackets:
+                if len(stack) == 0 or stack[-1] != closing_brackets[i]:
+                    return False
+                stack.pop()
+            else:
+                return False
+
+        return len(stack) == 0
