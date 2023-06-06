2a10548147f62cfd7bb19186ee0cf96fc421dd8c
JooIcksoo <dlrtn751@gmail.com>
2023-06-06 22:17:37 +0900

    2023. 06. 06.

---
+++ leetcode/16. 3Sum Closest.py
@@ -0,0 +1,26 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+
+        nums.sort()
+        answer = 0
+        result = 1000000000
+        for i in range(len(nums) - 2):
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                s = nums[i] + nums[left] + nums[right]
+
+                m = abs(target - s)
+                if m < result:
+                    answer = s
+                    result = m
+                if s > target:
+                    right -= 1
+
+                elif s < target:
+                    left += 1
+
+                else:
+                    return target
+        return answer
