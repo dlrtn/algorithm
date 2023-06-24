e599a82c0c728aeefc6f969e96a8a47dc023218f
dlrtn <87226112+dlrtn@users.noreply.github.com>
2023-06-24 20:06:06 +0900

    2023. 05. 05.

--- programmers/algorithm/과일장수.py
+++
@@ -1,9 +0,0 @@
-def solution(k, m, score):
-    answer = 0
-
-    score = sorted(score, reverse=True)
-
-    for i in range(len(score) // m):
-        answer += min(score[i * m:i * m + m]) * m
-
-    return answer
