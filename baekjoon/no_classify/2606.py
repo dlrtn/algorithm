70eb9643e815310c1496cb9083dc33268fb2da33
JooIcksoo <dlrtn751@gmail.com>
2023-07-08 23:14:10 +0900

    2023. 07. 08.

--- baekjoon/no_classify/2606.py
+++ baekjoon/no_classify/2606.py
@@ -1,14 +1,34 @@
 N = int(input())
 
 lst = [[0] * N for _ in range(N)]
-print(lst)
 
+graph = {}
 T = int(input())
+if T == 0:
+    print(0)
+    exit()
 for i in range(T):
     a, b = map(int, input().split())
 
-    lst[a - 1][b - 1] = 1
-    lst[b - 1][a - 1] = 1
+    if a not in graph.keys():
+        graph[a] = []
+    graph[a].append(b)
+    if b not in graph.keys():
+        graph[b] = []
+    graph[b].append(a)
 
-for i in range(N):
-    print(lst[i])
+
+def dfs(graph, start_node):
+    visited = []
+    stack = [start_node]
+
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.append(node)
+            stack.extend(graph[node])
+
+    return visited
+
+
+print(len(dfs(graph, 1)) - 1)
