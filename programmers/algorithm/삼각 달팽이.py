662530fac8c4670ed63d6216e28fdc2b73a33652
dlrtn <dlrtn751@gmail.com>
2023-10-13 13:32:32 +0900

    2023. 10. 13.

--- programmers/algorithm/삼각 달팽이.py
+++ programmers/algorithm/삼각 달팽이.py
@@ -1,36 +1,38 @@
 def solution(n):
-    answer = []
+    answer = [[0 for _ in range(i + 1)] for i in range(n)]
 
-    arr = [[] for i in range(n)]
-    control = 0
+    x, y = 0, 0
     count = 1
-    x = -1
-    for i in range(n, 0, -1):
-        for j in range(i):
-            print(x)
-            if control == 0:
-                if x < n - 1:
-                    x += 1
-                arr[x].append(count)
+    for i in range(n):
+        if i % 3 == 0:
+            for j in range(n - i):
+                answer[x][y] = count
+                count += 1
+                x += 1
+            x -= 1
+            y += 1
 
-            elif control == 1:
-                arr[x].append(count)
+        elif i % 3 == 1:
+            for j in range(n - i):
+                answer[x][y] = count
+                count += 1
+                y += 1
+            x -= 1
+            y -= 2
 
-            elif control == 2:
-                if x >= 0:
-                    x -= 1
-                arr[x].append(count)
-            count += 1
-
-        if control == 0:
-            control = 1
-        elif control == 1:
-            control = 2
         else:
-            control = 0
-        print(i)
+            for j in range(n - i):
+                answer[x][y] = count
+                count += 1
+                x -= 1
+                y -= 1
+            x += 2
+            y += 1
 
-        print(arr)
-    return answer
+    answer_list = []
 
-solution(5)
+    for i in answer:
+        while i:
+            answer_list.append(i.pop(0))
+
+    return answer_list
