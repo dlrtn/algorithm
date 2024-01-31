c2076839b21c8388a241d452c5f334667a479f86
JooIcksoo <dlrtn751@gmail.com>
2024-01-31 23:12:50 +0900

    2024. 01. 31.

---
+++ src/Camping.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int caseNumber = 1;
+        while (true) {
+            int L = sc.nextInt();
+            int P = sc.nextInt();
+            int V = sc.nextInt();
+
+            if (L == 0 && P == 0 && V == 0) {
+                break;
+            }
+
+            int usedDayCount = (V / P) * L;
+            usedDayCount += (V % P) > L ? L : (V % P);
+
+            System.out.println("Case " + caseNumber + ": " + usedDayCount);
+            caseNumber++;
+        }
+    }
+}
