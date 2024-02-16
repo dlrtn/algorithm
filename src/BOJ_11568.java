86ed1ebc6da619863d1d0491cc6444512bbdbe11
JooIcksoo <dlrtn751@gmail.com>
2024-02-16 17:51:49 +0900

    2024. 02. 16.

---
+++ src/BOJ_11568.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_11568 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int n = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.stream(arr).forEach(System.out::println);
+
+    }
+
+}
