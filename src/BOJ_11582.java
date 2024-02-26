3f05ffbc655698a7f9a2f4e910c0d56225fdaf31
dlrtn <dlrtn751@gmail.com>
2024-02-26 12:22:33 +0900

    2024. 02. 26.

---
+++ src/BOJ_11582.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_11582 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        OutputStream out = System.out;
+        OutputStreamWriter writer = new OutputStreamWriter(out);
+
+        int n = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        int k = Integer.parseInt(st.nextToken());
+
+        int size = n / k;
+
+        for (int i = 0; i < n; i += size) {
+            Arrays.stream(Arrays.copyOfRange(arr, i, i + size)).sorted().forEach((number) -> {
+                try {
+                    writer.write(number + " ");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        writer.flush();
+    }
+}
---
+++ src/BOJ_6198.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BOJ_6198 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        Stack<Integer> stack = new Stack<>();
+
+        Long count = 0L;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int input = Integer.parseInt(st.nextToken());
+
+            while (!stack.empty() && stack.peek() <= input) {
+                stack.pop();
+            }
+            count += stack.size();
+
+            stack.push(input);
+
+        }
+
+        System.out.print(count);
+    }
+
+}
