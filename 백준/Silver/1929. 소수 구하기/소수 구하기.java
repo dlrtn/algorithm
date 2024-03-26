a4de2f1ddd61a233bf271222b24ca85c70e5ec68
Icksoo <87226112+dlrtn@users.noreply.github.com>
2024-03-26 13:54:12 +0900

    [Silver III] Title: 소수 구하기, Time: 972 ms, Memory: 31744 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        boolean[] array = eratosthenes(n);
+
+        for (int i = m; i < n + 1; i++) {
+            if (array[i]) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrimeNumber(int n) {
+        if (n == 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public static boolean[] eratosthenes(int end) {
+        boolean[] array = new boolean[end + 1];
+        for (int i = 2; i < end + 1; i++) {
+            array[i] = true;
+        }
+
+        for (int i = 2; i < end + 1; i++) {
+            if (array[i] && isPrimeNumber(i)) {
+                // 걸러주는 로직
+                for (int j = i + i; j <= end; j += i) {
+                    array[j] = false;
+                }
+            }
+        }
+
+        return array;
+    }
+}
